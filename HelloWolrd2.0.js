a96e65d28efaaa8711c37ea1bc398503613f5180
Antkilla <132300802+Antkilla@users.noreply.github.com>
2023-08-18 18:11:24 -0700

    exercise 4
    numbers

--- HelloWolrd2.0.js
+++ HelloWolrd2.0.js
@@ -9,9 +9,9 @@ console.log(); //change this line
 console.log("My name is: Anton");
 console.log(); //change this line
 console.log("My age is: 21");
-console.log(); //change this line, which type should you use?
+console.log(); //change this line, which type should you use? -string 
 console.log("My zip code is: 90250"); 
-console.log(); //change this line, which type should you use?
-console.log(); //change this line, which type should you use?
+console.log(); //change this line, which type should you use?-string 
+console.log(); //change this line, which type should you use?-string 
 console.log("My zip code is: 90503"); 
-console.log(); //change this line, which type should you use?
+console.log(); //change this line, which type should you use?-string
