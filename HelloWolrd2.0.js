86513ab656ad9dea3257a53843ee11a2715bc546
Antkilla <132300802+Antkilla@users.noreply.github.com>
2023-08-18 17:52:43 -0700

    hellow world 2.0 Strings
    the usages of strings

---
+++ HelloWolrd2.0.js
@@ -0,0 +1,17 @@
+//EX.3 Strings And Anumbers
+
+console.log("This is a string, it has quotes.")
+console.log(100)
+console.log("100")
+
+console.log("My name is: Anthony ");
+console.log(); //change this line
+console.log("My name is: Anton");
+console.log(); //change this line
+console.log("My age is: 21");
+console.log(); //change this line, which type should you use?
+console.log("My zip code is: 90250"); 
+console.log(); //change this line, which type should you use?
+console.log(); //change this line, which type should you use?
+console.log("My zip code is: 90503"); 
+console.log(); //change this line, which type should you use?
--- HelloWorld.js
+++ HelloWorld.js
@@ -1,12 +1,18 @@
 //This is my first program!
+//Display hello world to the console//
 console.log('Hello World!');
+
+//EX.1//
 console.log('There once of was a writer of code')
 console.log('who wanted to learn js and node')
 console.log('They said, "Once I can learn,"')
 console.log("'I'll increase what I earn.'");
 console.log("So now they're in full programmer mode!");
 
+//Ex.2 
 
-
-
-// This is going to be a hard restart to do beccause of the instructions of direction 
+/*
+console.log("But this will not be displayed.");
+console.log("And neither will this");
+console.log("Or this");
+*/
