175733ce295665cc4577436e6d7bef5b81c7e58c
Antkilla <132300802+Antkilla@users.noreply.github.com>
2023-08-18 17:14:10 -0700

    Update HelloWorld.js
    The writer of code theme

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,6 +1,10 @@
 //This is my first program!
 console.log('Hello World!');
-
+console.log('There once of was a writer of code')
+console.log('who wanted to learn js and node')
+console.log('They said, "Once I can learn,"')
+console.log("'I'll increase what I earn.'");
+console.log("So now they're in full programmer mode!");
 
 
 
